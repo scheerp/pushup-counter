333739c01ed9536157ca9bc63a72a26ffcdc6b33
scheerp <patrick-scheer@web.de>
2020-02-15 13:21:17 +0100

    all chart

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GetData } from '../../services/GetData';
+import { GetData, GetRanking } from '../../services/GetData';
 import { Redirect } from 'react-router-dom';
 import Notification from '../Notification/Notification';
 import { Line } from 'react-chartjs-3';
@@ -12,6 +12,11 @@ export default class Stats extends React.Component {
             userData: '',
             sessionIsActive: true,
             lineGraphData: {
+                labels: [],
+                datasets: [
+                ]
+            },
+            lineGraphDataAll: {
                 labels: [],
                 datasets: [
                 ]
@@ -40,15 +45,65 @@ export default class Stats extends React.Component {
                         borderWidth: 1,
                         data: 0
                     }];
-                result.daily.forEach(element => {
+                result.daily.forEach((element, index) => {
                     let date = new Date(element.date);
+
                     days.push(`${date.getDate()} Feb`);
 
-                    pushups.push(element.pushups);
+                    if (date.getDate() === index + 1) {
+                        pushups.push(element.pushups);
+                    } else {
+                        pushups.push(0);
+                    }
                 });
 
                 data[0].data = pushups;
                 this.setState({ lineGraphData: { labels: days, datasets: data } });
+            }
+        }, (error) => {
+            this.displayNotifications(`Something went wrong: ${error}`, 'error');
+            console.error("Something went wrong:", error);
+        });
+
+        GetRanking(params.get('id')).then((result) => {
+            let responseJson = result;
+            let days = [];
+            let dataArray = [];
+            if (responseJson) {
+                result.forEach(user => {
+                    let pushups = [];
+                    let data =
+                    {
+                        label: user.username,
+                        fill: false,
+                        lineTension: 0,
+                        backgroundColor: 'rgba(75,192,192,1)',
+                        borderColor: 'rgba(255,255,255,1)',
+                        borderWidth: 1,
+                        data: []
+                    };
+                    user.daily.forEach((element, index) => {
+                        let date = new Date(element.date);
+
+                        index === 0 && days.push(`${date.getDate()} Feb`);
+                        console.log('index + 1:', index + 1)
+                        console.log('date:', element.date.slice(-2))
+
+                        if (element.date.slice(-2) == index + 1) {
+                            pushups.push(element.pushups);
+                        } else {
+                            pushups[index] = {};
+                            pushups.push(0);
+                        }
+
+                        data.data = pushups;
+                    });
+                    dataArray.push(data);
+
+                })
+                console.log('dataArray', dataArray)
+                this.setState({ lineGraphDataAll: { datasets: dataArray, labels: days } });
+
             }
         }, (error) => {
             this.displayNotifications(`Something went wrong: ${error}`, 'error');
@@ -82,6 +137,14 @@ export default class Stats extends React.Component {
                         display: true,
                         position: 'bottom'
                     }} />
+                    {console.log('tableDataAll:', this.state.lineGraphDataAll)}
+                    {console.log('tableData:', this.state.lineGraphData)}
+
+                    <Line data={this.state.lineGraphDataAll} legend={{
+                        display: true,
+                        position: 'bottom'
+                    }} />
+
                 </div>
             </>
         );
